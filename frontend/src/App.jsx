c95f46ca651c70a518ce718f7f932e1fa891df8e
3asem0153 <3asempro@gmail.com>
2025-05-18 09:27:33 +0300

    nav vw

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,23 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import SignIn from "./SignIn"
 import Home from './Home';
 import SignUp from "./SignUp";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 const App = () => {
   return <BrowserRouter>
- {/* <nav className="navbar bg-body-tertiary">
-  <div className="container-fluid">
-    <form className="d-flex" role="search">
-      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-outline-success" type="button">Search</button>
-       <button className="btn btn-outline-success" type="button">Dark Mode</button>
-    </form>
-  </div>
-</nav> */}
-<nav className="nav">
 
-</nav>
+  <div className="nav"></div>
+
     <Routes>
       <Route path='/' element={<SignIn />} />
       <Route path='/home/:id' element={<Home />} />
